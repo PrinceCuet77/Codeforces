e90cfe068cf14aeaea8f68e6fc4ebea1bbf3334f
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-04-13 21:50:27 +0600

    April 13, 2020

---
+++ C++/A/1335A Candies and Two Sisters.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std; 
+ 
+typedef long long ll;
+ 
+int main()
+{
+        //freopen("file.txt", "r", stdin);
+        ll t;       
+        cin >> t; 
+        while ( t-- ) {
+                ll n; 
+                cin >> n; 
+                if ( n <= 2 ) 
+                        cout << 0 << endl; 
+                else {
+                        if ( n % 2 == 0 ) 
+                                cout << n/2 - 1 << endl; 
+                        else 
+                                cout << n/2 << endl;
+                }
+        }
+        
+        return 0;
+}
