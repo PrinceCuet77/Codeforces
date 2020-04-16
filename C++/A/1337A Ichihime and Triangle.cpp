7a6b05f1acc2c55635e2d73ab7fc8e287254a5fa
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-04-16 12:27:41 +0600

    April 16, 2020

---
+++ C++/A/1337A Ichihime and Triangle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+        //freopen("file.txt", "r", stdin);
+        ll t;
+        scanf("%lld", &t);
+        while ( t-- ) {
+                ll a, b, c, d;
+                scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+                printf("%lld %lld %lld\n", a, c, c);
+        }
+
+        return 0;
+}
