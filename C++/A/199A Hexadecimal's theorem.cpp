f0763f92d869912da0623c2f5a8f43767d66b804
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-05-04 17:32:11 +0600

    May 04, 2020

---
+++ C++/A/199A Hexadecimal's theorem.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+        //freopen("file.txt", "r", stdin);
+        ll n;
+        while ( cin >> n ) 
+                cout << "0 0 " << n << endl; 
+
+        return 0;
+}
