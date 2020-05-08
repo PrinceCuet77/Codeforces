ae0cfb61c0f1ac243d220460a54844e9e5f25ecd
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-05-08 06:02:50 +0600

    May 08, 2020

---
+++ C++/A/1351A A+B (Trial Problem).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        //freopen("file.txt", "r", stdin);
+        int tc;
+        cin >> tc;
+        while ( tc-- ) {
+                int a, b;
+                cin >> a >> b;
+
+                cout << a + b << endl;
+        }
+
+        return 0;
+}
