bd1fc0d2cc1d670bd8b5f58dc3d7c2caffe7a6c7
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-05-19 16:05:55 +0600

    May 20, 2020

---
+++ C++/A/1325A EhAb AnD gCd.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    cout << 1 << " " << n-1 << endl;
+}
+
+int main() 
+{
+    int tc; 
+    cin >> tc; 
+
+    while ( tc-- ) 
+        solve();
+
+    return 0;
+}
