4da4b713e914bb7eddffae6d9641cf244ea90ca4
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-05-26 21:47:45 +0600

    May 26, 2020

---
+++ C++/A/1358A Park Lighting.cpp
@@ -0,0 +1,43 @@
+/********************************************************************************
+*                                                                               *
+*        Author     : Rezoan Shakil Prince                                      *
+*        Batch      : 14                                                        *
+*        Email      : prince.cuet.77@gmail.com                                  *
+*        University : Chittagong University of Engineering and Technology       *
+*                                                                               *
+********************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll; 
+
+void solve() {
+        ll n, m; 
+        while ( cin >> n >> m ) {
+                ll t = n * m;
+                if ( t & 1 ) 
+                        cout << (ll)(t/2)+1 << "\n";
+                else 
+                        cout << (ll)(t/2) << "\n";
+        }
+}
+
+int main()
+{
+        ios_base :: sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        int tc;
+        cin >> tc;
+        while ( tc-- )
+                solve();
+
+        return 0;
+}
