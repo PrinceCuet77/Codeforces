a07b098fbd5387cd2776bb45bad8d51058f08652
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-05-26 07:34:50 +0600

    May 26, 2020

---
+++ C++/A/1328A Divisibility Problem.cpp
@@ -0,0 +1,45 @@
+/********************************************************************************
+*                                                                               *
+*        Author     : Rezoan Shakil Prince                                      *
+*        Batch      : 14                                                        *
+*        Email      : prince.cuet.77@gmail.com                                  *
+*        University : Chittagong University of Engineering and Technology       *
+*                                                                               *
+********************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll; 
+
+void solve() {
+        ll a, b; 
+        while ( cin >> a >> b ) {
+                if ( a % b == 0 ) {
+                        cout << "0\n";
+                        return;
+                }
+
+                ll t = a % b; 
+                cout << abs(b - t) << "\n";
+        }
+}
+
+int main()
+{
+        ios_base :: sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        int tc;
+        cin >> tc;
+        while ( tc-- )
+                solve();
+
+        return 0;
+}
