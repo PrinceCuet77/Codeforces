ac663f9c996854666548eadae8fc4cf5ef21fba1
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-06-10 19:12:21 +0600

    June 10, 2020

---
+++ C++/B/451B Sort the Array.cpp
@@ -0,0 +1,68 @@
+/********************************************************************************
+*                                                                               *
+*        Author     : Rezoan Shakil Prince                                      *
+*        Batch      : 14                                                        *
+*        Email      : prince.cuet.77@gmail.com                                  *
+*        University : Chittagong University of Engineering and Technology       *
+*                                                                               *
+********************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() { // 451B Sort the Array.cpp
+        int n; 
+        while ( cin >> n ) {
+                int a[n], b[n]; 
+                for ( int i = 0; i < n; i++ ) {
+                        cin >> a[i]; 
+                        b[i] = a[i];
+                }
+
+                sort(b, b+n);
+
+                int l = 0, r = n-1;
+                for ( ; l < n && a[l] == b[l]; l++ );
+                for ( ; r >= 0 && a[r] == b[r]; r-- );
+
+                bool flag = false;
+                for ( int i = l; i < r; i++ ) {
+                        if ( a[i] < a[i+1] ) {
+                                flag = true; 
+                                break;
+                        }
+                }
+
+                if ( l == n ) {
+                        cout << "yes\n"; 
+                        cout << "1 1\n"; 
+                        continue;
+                }
+
+                if ( flag ) 
+                        cout << "no\n";
+                else {
+                        cout << "yes\n"; 
+                        cout << l+1 << " " << r+1 << endl; 
+                }
+        }
+}
+
+int main()
+{
+        ios_base :: sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        // int tc;
+        // cin >> tc;
+        // while ( tc-- )
+                solve();
+
+        return 0;
+}
