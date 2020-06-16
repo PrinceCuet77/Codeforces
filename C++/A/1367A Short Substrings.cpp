5e22aa5cdd0464b503136eb372e7f4cd8e5ed9ec
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-06-17 01:33:23 +0600

    June 17, 2020

---
+++ C++/A/1367A Short Substrings.cpp
@@ -0,0 +1,42 @@
+/********************************************************************************
+*                                                                               *
+*        Author     : Rezoan Shakil Prince                                      *
+*        Batch      : 14                                                        *
+*        Email      : prince.cuet.77@gmail.com                                  *
+*        University : Chittagong University of Engineering and Technology       *
+*                                                                               *
+********************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+void solve() {  
+        string ch; 
+        cin >> ch;
+        cout << ch[0]; 
+
+        for ( int i = 1; i < ch.length() - 2; i += 2 ) 
+                cout << ch[i];
+
+        cout << ch[ch.length()-1] << endl; 
+}
+
+int main()
+{
+        IOS;
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        int tc;
+        cin >> tc;
+        while ( tc-- )
+                solve();
+
+        return 0;
+}
