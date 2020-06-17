3c4009bffe392cb306b8541ca6e3e606b1444f4c
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-06-18 02:29:04 +0600

    June 18, 2020

---
+++ C++/A/1283A Minutes Before the New Year.cpp
@@ -0,0 +1,38 @@
+/********************************************************************************
+*                                                                               *
+*        Author     : Rezoan Shakil Prince                                      *
+*        Batch      : 14                                                        *
+*        Email      : prince.cuet.77@gmail.com                                  *
+*        University : Chittagong University of Engineering and Technology       *
+*                                                                               *
+********************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+void solve() {  // 1283A 
+        int h, m; 
+        cin >> h >> m; 
+
+        cout << (23 - h) * 60 + (60 - m) << endl;
+}
+
+int main()
+{
+        IOS;
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        int tc;
+        cin >> tc;
+        while ( tc-- )
+                solve();
+
+        return 0;
+}
