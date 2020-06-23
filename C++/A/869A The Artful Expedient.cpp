f03fdaf53b1adaf965c036f00ce9acbe8089ed26
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-06-23 14:15:14 +0600

    June 23, 2020

---
+++ C++/A/869A The Artful Expedient.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Prince ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+void solve() {
+        int n; 
+        while ( cin >> n ) {
+                int a; 
+                for ( int i = 0; i < n; i++ ) cin >> a; 
+                for ( int i = 0; i < n; i++ ) cin >> a;
+
+                cout << "Karen" << endl;        
+        }      
+}
+
+int main()
+{
+        Prince;
+
+        // #ifndef prince77
+        //         freopen("input.txt", "r", stdin);
+        //         freopen("output.txt", "w", stdout);
+        // #endif
+
+        solve();
+
+        return 0;
+}
