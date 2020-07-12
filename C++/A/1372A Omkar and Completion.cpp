3442aacba423ea15c47621256d15f6cca5d094c7
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2020-07-12 09:02:36 +0600

    July 11, 2020

---
+++ C++/A/1372A Omkar and Completion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {  
+        int n; 
+        cin >> n; 
+        for ( int i = 1; i <= n; i++ )
+                cout << 1 << " ";
+        cout << endl; 
+}
+
+int main()
+{
+        int tc;
+        cin >> tc;
+        while ( tc-- )
+                solve();
+
+        return 0;
+}
